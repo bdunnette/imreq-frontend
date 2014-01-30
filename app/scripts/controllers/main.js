5e13d5b05890f9f59f85665e703ca882bb4bea20
Brian Dunnette <dunn0172@umn.edu>
2014-01-30 10:24:26 -0600

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('imreqFrontendApp')
+  .controller('MainCtrl', function ($scope, $resource) {
+    $scope.formData = {};
+    var ImageRequest = $resource('http://160.94.51.184:1337/imagingrequest/:requestId');
+    //var ImageRequest = $resource('http://127.0.0.1:1337/imagingrequest/:requestId');
+    var requests = ImageRequest.query(function () {
+        $scope.requests = requests;
+    });
+    
+    $scope.processForm = function () {
+        var newRequest = new ImageRequest($scope.formData);
+        newRequest.$save();
+        $scope.formData = {};
+    };
+  });
