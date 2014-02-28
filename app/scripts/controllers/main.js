019aac132f4d7483d27833b1ce11cc14ad67ad6f
Brian Dunnette <dunn0172@umn.edu>
2014-02-28 16:31:59 -0600

    adding delete function; updating grid styles

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,11 +9,31 @@ angular.module('imreqFrontendApp')
         $scope.requests = requests;
     });
     
-    $scope.gridOptions = { data: 'requests' };
+    $scope.gridOptions = { 
+        data: 'requests',
+        columnDefs: [
+            {field:'id', displayName:'Request ID'},
+            {field:'specimenID', displayName:'Specimen ID'},
+            {field:'modality', displayName:'Imaging Type'},
+            {field:'requestor', displayName:'Requestor', width:'**'},
+            {field:'status', displayName:'Status', cellTemplate: '<div ng-class="{green: row.getProperty(col.field) == \'uploaded\'}"><div class="ngCellText">{{row.getProperty(col.field)}}</div></div>'},
+            {field:'updatedAt', displayName:'Last Updated', width:'**'},
+            {field:'delete', displayName:'Delete', cellTemplate: '<div ng-click=removeRequest(row)><span class="glyphicon glyphicon-remove-circle"></span></div>'}
+        ],
+        sortInfo: {fields:['updatedAt'], directions:['desc']}
+    };
       
     $scope.processForm = function () {
         var newRequest = new ImageRequest($scope.formData);
+        $scope.requests.push(newRequest);
         newRequest.$save();
         $scope.formData = {};
+    };
+      
+    $scope.removeRequest = function(row) {
+        var index = $scope.requests.indexOf(row.entity);
+        $resource('http://160.94.51.184:1337/imagingrequest/' + row.entity.id).delete(function (response) {
+            $scope.requests.splice(index, 1);
+        });
     };
   });
